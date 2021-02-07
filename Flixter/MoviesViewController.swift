88f341cbab65d304332a91bf14414da24461685f
winnie1027 <71732749+winnie1027@users.noreply.github.com>
2021-02-06 20:03:42 -0600

    again

--- Flixter/MoviesViewController.swift
+++ Flixter/MoviesViewController.swift
@@ -65,7 +65,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         
-        cell.titleLabel.text = title 
+        let synopsis = movie["overview"] as! String
+        
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
         
         return cell
     }
