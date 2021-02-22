ecc74b92676f5eb27d40373333d3ca833e65fceb
winnie1027 <71732749+winnie1027@users.noreply.github.com>
2021-02-21 21:15:24 -0600

    to save

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Winnie Yang on 2/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+}
